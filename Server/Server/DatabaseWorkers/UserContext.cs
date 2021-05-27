39a13cec83435486e59245d6cfc0ae12addad7c5
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-28 02:09:34 +0500

    Реализована система регистрации пользователей и сохранения аватар-изображений со стороны сервера

---
+++ Server/Server/DatabaseWorkers/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.DatabaseWorkers
+{
+    public sealed class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        // public UserContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Filename=users.db");
+    }
+}
---
+++ Server/Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string AvatarFilePath { get; set; }
+    }
+}
