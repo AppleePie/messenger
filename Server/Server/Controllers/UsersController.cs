ef78f1cda52f89908f7c6243c3f6584c5f2e9b82
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-28 02:31:19 +0500

    Реализована система регистрации пользователей и сохранения аватар-изображений со стороны сервера

---
+++ Server/Server/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.DatabaseWorkers;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/users/")]
+    public class UsersController : Controller
+    {
+        [HttpGet("{id:guid}")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetUserByIdAsync([FromRoute] Guid id) =>
+            Ok(await UserRepository.FindByIdAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserAsync([FromBody] User user)
+        {
+            if (user is null)
+                return BadRequest("User body should not be null or empty!");
+
+            if (await UserRepository.FindByNameAsync(user.Name) != null)
+                return Conflict("User with this username already exists!");
+
+            var insertedUserId = await UserRepository.InsertAsync(user);
+            return Ok(insertedUserId);
+        }
+
+        [HttpPost("{id:guid}/avatar")]
+        public async Task<IActionResult> AddAvatar([FromRoute] Guid id, IFormFileCollection uploads)
+        {
+            if (uploads.Count != 1)
+                return BadRequest("Avatar file collection should contains only one element!");
+
+            var uploadDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+            if (!Directory.Exists(uploadDirectory)) 
+                Directory.CreateDirectory(uploadDirectory);
+
+            var uploadedFile = uploads[0];
+            var path = Path.Combine(uploadDirectory, uploadedFile.FileName);
+            await using var fileStream = new FileStream(path, FileMode.Create);
+            await uploadedFile.CopyToAsync(fileStream);
+
+            await UserRepository.ChangeUserAvatarAsync(id, uploadedFile.FileName);
+            return NoContent();
+        }
+    }
+}
---
+++ Server/Server/DatabaseWorkers/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.DatabaseWorkers
+{
+    public static class UserRepository
+    {
+        private static readonly UserContext Database = new();
+
+        public static async Task<User> FindByIdAsync(Guid userId)
+        {
+            var result = await Database.Users.FindAsync(userId);
+            await Database.SaveChangesAsync();
+            return result;
+        }
+
+        public static async Task<Guid> InsertAsync(User user)
+        {
+            await Database.AddAsync(user);
+            await Database.SaveChangesAsync();
+            return user.Id;
+        }
+
+        public static async Task ChangeUserAvatarAsync(Guid userId, string filePath)
+        {
+            var user = await FindByIdAsync(userId);
+            user.AvatarFilePath = filePath;
+            Database.Update(user);
+            await Database.SaveChangesAsync();
+        }
+
+        public static async Task<User> FindByNameAsync(string userName)
+        {
+            var result = await Database.Users.FirstOrDefaultAsync(user => user.Name == userName);
+            await Database.SaveChangesAsync();
+            return result;
+        }
+    }
+}
